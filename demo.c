dacca65a04ec5705c963b5a222fe71541d53526f
yalda <yalda@kako.cc>
2020-01-22 01:30:17 -0600

    initial checkin

---
+++ demo.c
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <jni.h>
+#include <stdio.h>
+#include "cc_kako_examples_jni_Main.h"
+
+JNIEXPORT void JNICALL Java_cc_kako_examples_jni_Main_nativePrint
+        (JNIEnv *env, jobject obj)
+{
+
+    printf("\nHello World from C\n");
+
+}
